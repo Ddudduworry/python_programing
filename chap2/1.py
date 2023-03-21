7a4967f6e035a06d764446bf472afe1d25311195
honggildong <hong@dongyang.ac.kr>
2023-03-21 12:43:26 +0900

    다함

---
+++ chap2/1.py
@@ -0,0 +1,27 @@
+#1.py
+'''이것은 여러줄로
+이루어진 주석입니다'''
+
+
+print("hello world!!")
+print("파이썬" + "만세")
+print("파이썬" + "만" +"세"+"!!")
+print("파이썬!!!" *3)
+
+print("hello 'world'!!")
+print('hello "world"!!')
+print(type("hello"))
+print(type(1.1))
+
+print(1,2,-5,3.14,2.71828)
+print('hi','python')
+print('23000원','5000원 ?개', '1000원 ?개')
+print('5000원',23000//5000,'개')
+print('1000원',(23000%5000)//1000,'개')
+print(eval('3+15/2'))
+print(eval('4*3%5'))
+
+print(eval('1+62-3*52'))
+print(256*553-152)
+
+print(eval('1632665%445')*eval('1632665/445'))
