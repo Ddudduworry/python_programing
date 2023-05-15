e444c2266682f9859b2e8dc9d34d089367783be4
김유정 <tlthrn2@naver.com>
2023-05-15 09:51:44 +0900

    ZZ

---
+++ practice/practice.py
@@ -0,0 +1,38 @@
+print("python""programing")
+print("\n\n------------")
+
+print('''뭐가 신기해
+앙 그렇냐구''')
+#이게 주석인가여??
+
+print(divmod(45,6))
+print(5/10/2)
+
+days = 257
+months = days //30
+days %= 30
+print(months, '달',days,'일')
+#input('대학은?')
+
+a=5432%10
+b=543%10
+c=54%10
+d=5%10
+print(a,b,c,d)
+
+#number = int(input('아메리카노 몇 개 주문하세요?'))
+#price = number*3500
+#print(number,price)
+
+print('->'.join('1345'))
+
+print('{} {}'.format('고도비만',40 <= 50))
+
+for i in range(2,10):
+    print("\n")
+    for j in range(1,10):
+        print('%d * %d = %2d' %(i,j,i*j),end=' ')
+
+
+dd= 'dgds'
+print(dd.count('d'))
